a664acdf5627a3afb24cd41ca752186c9dfb4ece
Tomasz Swistak <tomasz.swistak@synergycodes.com>
2025-04-28 14:01:15 +0200

    Enables node movement via pointer events
    Adds the ability to move selected nodes using pointer events (mouse/touch).
    
    This introduces a new action that listens for pointer events and emits a 'moveSelection' command with the appropriate delta values.
    This makes it possible to drag and drop nodes.

--- packages/logger-middleware/eslint.config.js
+++ packages/logger-middleware/eslint.config.js
@@ -4,6 +4,6 @@ import prettier from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-  files: ['**/*.ts'],
+  files: ['src/**/*.ts'],
   extends: [eslint.configs.recommended, ...tseslint.configs.recommended, ...tseslint.configs.stylistic, prettier],
 });
