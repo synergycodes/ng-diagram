270983fba1d460e48cfd477b58354a418af92bf1
Mateusz Dropiński <mateusz.dropinski@synergycodes.com>
2025-05-06 15:00:12 +0200

    Add working markers to edges

--- packages/angular-adapter/projects/angular-adapter/src/lib/components/edge/markers/marker-arrow.component.ts
+++ packages/angular-adapter/projects/angular-adapter/src/lib/components/edge/markers/marker-arrow.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'angular-adapter-marker-arrow',
   templateUrl: './marker-arrow.component.html',
   styleUrl: './marker-arrow.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class MarkerArrowComponent {}
