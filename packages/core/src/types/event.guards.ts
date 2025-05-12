2b8ceac16ff2815dcd933e49d7a42648be4f75e0
Mateusz Dropiński <mateusz.dropinski@synergycodes.com>
2025-05-12 13:23:38 +0200

    Add new event interface

--- packages/core/src/types/event.guards.ts
+++ packages/core/src/types/event.guards.ts
@@ -1,32 +1,36 @@
 import {
   Event,
+  KeyboardDownEvent,
   KeyboardEvent,
+  KeyboardPressEvent,
+  KeyboardUpEvent,
   PointerDownEvent,
   PointerEnterEvent,
   PointerEvent,
   PointerLeaveEvent,
   PointerMoveEvent,
   PointerUpEvent,
+  ResizeEvent,
 } from './event.interface';
 
 /**
  * Check if an event is a keyboard down event
  */
-export const isKeyboardDownEvent = (event: Event): event is KeyboardEvent => {
+export const isKeyboardDownEvent = (event: Event): event is KeyboardDownEvent => {
   return event.type === 'keydown';
 };
 
 /**
  * Check if an event is a keyboard up event
  */
-export const isKeyboardUpEvent = (event: Event): event is KeyboardEvent => {
+export const isKeyboardUpEvent = (event: Event): event is KeyboardUpEvent => {
   return event.type === 'keyup';
 };
 
 /**
  * Check if an event is a keyboard press event
  */
-export const isKeyboardPressEvent = (event: Event): event is KeyboardEvent => {
+export const isKeyboardPressEvent = (event: Event): event is KeyboardPressEvent => {
   return event.type === 'keypress';
 };
 
@@ -83,4 +87,11 @@ export const isPointerEvent = (event: Event): event is PointerEvent => {
     isPointerEnterEvent(event) ||
     isPointerLeaveEvent(event)
   );
+};
+
+/**
+ * Check if an event is a resize event
+ */
+export const isResizeEvent = (event: Event): event is ResizeEvent => {
+  return event.type === 'resize';
 };
