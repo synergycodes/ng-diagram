a52253a1bb288c0e30b9aff2fee774e0d5c643d1
Mateusz Dropiński <mateusz.dropinski@synergycodes.com>
2025-05-12 13:13:29 +0200

    Add size property to node

--- packages/core/src/types/node.interface.ts
+++ packages/core/src/types/node.interface.ts
@@ -2,12 +2,38 @@
  * Interface representing a node in the flow diagram
  */
 export interface Node {
+  /**
+   * The unique identifier for the node.
+   */
   id: string;
+  /**
+   * The position of the node in the diagram.
+   */
   position: {
     x: number;
     y: number;
   };
+  /**
+   * The data associated with the node.
+   */
   data: Record<string, unknown>;
+  /**
+   * The type of the node declared in nodeTemplateMap.
+   */
   type: string;
+  /**
+   * Whether the node is selected.
+   */
   selected?: boolean;
+  /**
+   * The size of the node.
+   */
+  size?: {
+    width: number;
+    height: number;
+    /**
+     * Whether the size of the node is controlled by the user.
+     */
+    controlled?: boolean;
+  };
 }
