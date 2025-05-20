a8ef34171c84744fd619c83ff8b5c28bc43a70f7
Dawid Aksamski <dawid.aksamski@synergycodes.com>
2025-05-20 12:16:22 +0200

    Simplify some directives implementations

--- packages/angular-adapter/projects/angular-adapter/src/lib/directives/node-selected/node-selected.directive.ts
+++ packages/angular-adapter/projects/angular-adapter/src/lib/directives/node-selected/node-selected.directive.ts
@@ -1,22 +1,12 @@
-import { Directive, effect, ElementRef, inject, input, Renderer2 } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: '[angularAdapterNodeSelected]',
+  host: {
+    '[style.transition]': '"box-shadow 0.1s ease-in-out"',
+    '[style.box-shadow]': 'selected() ? "0 0 2px 4px rgba(30, 144, 255, 0.5)" : "none"',
+  },
 })
 export class NodeSelectedDirective {
-  private readonly hostElement = inject(ElementRef<HTMLElement>);
-  private readonly renderer = inject(Renderer2);
-
   selected = input<boolean | undefined>(false);
-
-  constructor() {
-    this.setStyle('transition', 'box-shadow 0.1s ease-in-out');
-    effect(() => {
-      this.setStyle('box-shadow', this.selected() ? '0 0 2px 4px rgba(30, 144, 255, 0.5)' : 'none');
-    });
-  }
-
-  private setStyle(styleName: string, value: string) {
-    this.renderer.setStyle(this.hostElement.nativeElement, styleName, value);
-  }
 }
