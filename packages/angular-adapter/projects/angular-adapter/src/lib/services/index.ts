a1e842cb89e42b3269e497e724163b81967f71b5
Emil Małańczak <kdddmalanczak@gmail.com>
2025-06-24 18:30:59 +0200

    Fix exports

--- packages/angular-adapter/projects/angular-adapter/src/lib/services/index.ts
+++ packages/angular-adapter/projects/angular-adapter/src/lib/services/index.ts
@@ -1,6 +1,6 @@
 export * from './event-mapper/event-mapper.service';
 export * from './flow-core-provider/flow-core-provider.service';
 export * from './flow-resize-observer/batched-resize-observer.service';
-export * from './flow-resize-observer/flow-resize-procesor.service';
+export * from './flow-resize-observer/flow-resize-processor.service';
 export * from './renderer/renderer.service';
 export * from './update-ports/update-ports.service';
