5ccf51c824113edfa82779ec429759a43ff15b83
lukasz jazwa <lukasz.jazwa@synergycodes.com>
2025-07-09 01:30:02 +0200

    Updated tree-layout consts

--- packages/core/src/middleware-manager/middlewares/tree-layout/constants.ts
+++ packages/core/src/middleware-manager/middlewares/tree-layout/constants.ts
@@ -2,9 +2,9 @@
 import { TreeLayoutConfig } from '../../../types/tree-layout.interface.ts';
 
 export const TREE_LAYOUT_DEFAULT_CONFIG: TreeLayoutConfig = {
-  siblingGap: 20,
-  levelGap: 30,
-  layoutAngle: 90,
+  siblingGap: 50,
+  levelGap: 150,
+  layoutAngle: 0,
   layoutAlignment: 'Parent',
   autoLayout: false,
 };
