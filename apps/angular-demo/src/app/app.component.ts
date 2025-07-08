2a6a771ecfbc236c2737128ea9c0add4537ae871
lukasz jazwa <lukasz.jazwa@synergycodes.com>
2025-07-08 22:53:08 +0200

    Node styles alpha

--- apps/angular-demo/src/app/app.component.ts
+++ apps/angular-demo/src/app/app.component.ts
@@ -38,7 +38,7 @@ export class AppComponent {
         data: { imageUrl: 'https://tinyurl.com/bddnt44s' },
         resizable: true,
       },
-      { id: '2', type: 'input-field', position: { x: 400, y: 250 }, data: {}, resizable: true },
+      { id: '2', type: 'input-field', position: { x: 400, y: 250 }, data: {} },
       { id: '3', type: 'resizable', position: { x: 700, y: 200 }, data: {}, resizable: true, groupId: '2' },
       {
         id: '4',
--- apps/angular-demo/src/app/node-template/input-field-node/input-field-node.component.ts
+++ apps/angular-demo/src/app/node-template/input-field-node/input-field-node.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component, input, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AngularAdapterPortComponent, INodeTemplate, Node } from '@angularflow/angular-adapter';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { phosphorAngularLogo } from '@ng-icons/phosphor-icons/regular';
 
 @Component({
   selector: 'app-input-field-node',
-  imports: [FormsModule, AngularAdapterPortComponent],
+  imports: [FormsModule, AngularAdapterPortComponent, NgIcon],
+  providers: [provideIcons({ phosphorAngularLogo })],
   templateUrl: './input-field-node.component.html',
   styleUrls: ['./input-field-node.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
