6e929bd969d8f1db30d0e2ceb189ee2aa1595913
lukasz jazwa <lukasz.jazwa@synergycodes.com>
2025-07-09 00:28:27 +0200

    Group styles

--- apps/angular-demo/src/app/node-template/group-node/group-node.component.ts
+++ apps/angular-demo/src/app/node-template/group-node/group-node.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, computed, input } from '@angular/core';
 import { INodeTemplate, Node } from '@angularflow/angular-adapter';
 
 @Component({
@@ -12,4 +12,12 @@ export class GroupNodeComponent implements INodeTemplate {
   data = input.required<Node>();
   groupTitle = computed(() => this.data().data?.['title'] ?? 'Group');
   highlighted = computed(() => this.data().highlighted ?? false);
+
+  // Computed property to check if node is selected
+  readonly isSelected = computed(() => this.data().selected ?? false);
+
+  // Host binding to apply CSS class when selected
+  @HostBinding('class.node-selected') get nodeSelectedClass() {
+    return this.isSelected();
+  }
 }
