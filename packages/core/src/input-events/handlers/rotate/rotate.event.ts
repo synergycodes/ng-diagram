b179adc30bf419d4867fe294b68ad12e6fda8c82
Jakub Kubacki <jakub.kubacki@synergycodes.com>
2025-07-24 11:50:31 +0200

    [AF-134] Cleanup coordinates in rotate event

--- packages/core/src/input-events/handlers/rotate/rotate.event.ts
+++ packages/core/src/input-events/handlers/rotate/rotate.event.ts
@@ -3,7 +3,16 @@ import { BasePointerInputEvent } from '../../input-events.interface';
 
 export interface RotateInputEvent extends BasePointerInputEvent {
   name: 'rotate';
+  /**
+   * The point where the pointer event occured in client coordinates.
+   */
   lastInputPoint: Point;
+  /**
+   * The center of drag handler in the diagram coordinates.
+   */
   handle: Point;
+  /**
+   * The center of the node in the diagram coordinates.
+   */
   center: Point;
 }
