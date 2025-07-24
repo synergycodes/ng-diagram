486d17bfeb1d5dc449fa5e3779143d0b41558e5d
Jakub Kubacki <jakub.kubacki@synergycodes.com>
2025-07-24 11:08:40 +0200

    [AF-101] Include canvas offset

--- packages/core/src/input-events/handlers/paste/paste.handler.ts
+++ packages/core/src/input-events/handlers/paste/paste.handler.ts
@@ -3,6 +3,16 @@ import { PasteEvent } from './paste.event';
 
 export class PasteEventHandler extends EventHandler<PasteEvent> {
   handle(event: PasteEvent): void {
-    this.flow.commandHandler.emit('paste', event.lastInputPoint ? { position: event.lastInputPoint } : {});
+    this.flow.commandHandler.emit('paste', this.getPasteParameters(event));
+  }
+
+  private getPasteParameters(event: PasteEvent) {
+    if (!event.lastInputPoint) {
+      return {};
+    }
+
+    return {
+      position: this.flow.clientToFlowPosition(event.lastInputPoint),
+    };
   }
 }
