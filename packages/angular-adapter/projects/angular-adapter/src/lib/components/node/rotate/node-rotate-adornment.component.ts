809cc200cd5e5e8de01b623a6766ec816ee60dfc
Jakub Kubacki <jakub.kubacki@synergycodes.com>
2025-07-28 11:06:57 +0200

    [AF-136] Simplify rotate adornment api

--- packages/angular-adapter/projects/angular-adapter/src/lib/components/node/rotate/node-rotate-adornment.component.ts
+++ packages/angular-adapter/projects/angular-adapter/src/lib/components/node/rotate/node-rotate-adornment.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, computed, input, signal } from '@angular/core';
-import { Node } from '@angularflow/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { AngularAdapterNodeComponent } from '../angular-adapter-node.component';
 import { RotateHandleComponent } from './handle/rotate-handle.component';
 
 @Component({
@@ -13,8 +13,10 @@ import { RotateHandleComponent } from './handle/rotate-handle.component';
   },
 })
 export class NodeRotateAdornmentComponent {
+  private readonly nodeComponent = inject(AngularAdapterNodeComponent);
+
+  readonly nodeData = computed(() => this.nodeComponent.data());
   readonly isRotating = signal(false);
-  readonly data = input.required<Node>();
-  readonly eventTarget = computed(() => ({ type: 'rotate-handle' as const, element: this.data() }));
-  readonly showAdornment = computed(() => !!this.data().selected && this.data().rotatable);
+  readonly eventTarget = computed(() => ({ type: 'rotate-handle' as const, element: this.nodeData() }));
+  readonly showAdornment = computed(() => !!this.nodeData().selected && this.nodeData().rotatable);
 }
