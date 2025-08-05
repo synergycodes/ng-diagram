e8439e13310918e201d0a9573ec401e890a94147
Kamil Fogel <kamil.fogel@synergycodes.com>
2025-08-05 17:18:48 +0200

    AF-145 remove trace option for logger

--- packages/core/src/middleware-manager/middlewares/logger/logger.ts
+++ packages/core/src/middleware-manager/middlewares/logger/logger.ts
@@ -22,16 +22,6 @@ export const loggerMiddleware: Middleware<'logger', LoggerMiddlewareMetadata> = 
       initialUpdate: context.initialUpdate,
     });
 
-    /*
-     * Set window.trace to true to enable console.trace for specific debugging.
-     * This is useful for debugging specific actions without cluttering the console.
-     * Just set window.trace = true in the console to enable it.
-     */
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    if ((window as any)['trace']) {
-      console.trace(`[AngularFlow] ${context.modelActionType}`);
-    }
-
     next();
   },
 };
