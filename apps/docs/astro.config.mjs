dc7008b8198c746e10a8a5d2baa5c4f41b571cd0
Jan Librowski <jan.librowski@synergycodes.com>
2025-08-05 16:31:31 +0200

    Remove boilerplate from /docs

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -2,19 +2,15 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
-      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+      title: 'NgDiagram',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/synergycodes/angularflow' }],
       sidebar: [
         {
           label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', slug: 'guides/example' },
-          ],
+          items: [{ label: 'Example Guide', slug: 'guides/example' }],
         },
         {
           label: 'Reference',
