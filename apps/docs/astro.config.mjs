5e0b85f08c8cee0cf5c8a0ee34bcbc412f9bf8f0
Jan Librowski <jan.librowski@synergycodes.com>
2025-08-06 11:40:26 +0200

    Setup astro-angular

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -2,8 +2,17 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import angular from '@analogjs/astro-angular';
+
 export default defineConfig({
   integrations: [
+    angular({
+      vite: {
+        transformFilter: (_code, id) => {
+          return id.includes('src/components/angular');
+        },
+      },
+    }),
     starlight({
       title: 'NgDiagram',
       social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/synergycodes/angularflow' }],
