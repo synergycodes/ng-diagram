f73916f275dd9087ef1669067968fe0911c96fcf
Piotr Błaszczyk <105283001+piotrblaszczyk@users.noreply.github.com>
2025-08-18 14:32:12 +0200

    [AF-165] Introduce Properties sidebar example (#213)
    * [AF-165] Introduce Properties sidebar example
    
    Add description
    
    Improve styles
    
    * Minor fixes
    
    * Minor fix
    
    ---------
    
    Co-authored-by: Jan Librowski <jan.librowski@synergycodes.com>

---
+++ apps/docs/src/components/angular/examples/properties-sidebar/diagram.component.ts
@@ -0,0 +1,48 @@
+import '@angular/compiler';
+
+import { Component } from '@angular/core';
+import { NgDiagramComponent, NgDiagramContextComponent, type AppMiddlewares } from '@angularflow/angular-adapter';
+import { SidebarContainer } from './sidebar.component';
+import { createSignalModel } from '@angularflow/angular-signals-model';
+
+@Component({
+  imports: [NgDiagramContextComponent, NgDiagramComponent, SidebarContainer],
+  template: `
+    <ng-diagram-context>
+      <ng-diagram [model]="model" />
+      <sidebar-container />
+    </ng-diagram-context>
+  `,
+  styles: `
+    :host {
+      flex: 1;
+      display: flex;
+      height: 100%;
+
+      .coordinates {
+        display: flex;
+      }
+    }
+  `,
+})
+export class NgDiagramPropertiesSidebarContainer {
+  model = createSignalModel<AppMiddlewares>({
+    metadata: {
+      viewport: { x: -45, y: 80, scale: 0.88 },
+    },
+    nodes: [
+      { id: '1', position: { x: 100, y: 150 }, data: { label: 'Node 1' }, rotatable: true },
+      { id: '2', position: { x: 400, y: 150 }, data: { label: 'Node 2' } },
+    ],
+    edges: [
+      {
+        id: '1',
+        source: '1',
+        sourcePort: 'port-right',
+        targetPort: 'port-left',
+        target: '2',
+        data: {},
+      },
+    ],
+  });
+}
---
+++ apps/docs/src/components/angular/examples/properties-sidebar/sidebar.component.ts
@@ -0,0 +1,35 @@
+import '@angular/compiler';
+
+import { Component, computed, inject } from '@angular/core';
+import { NgDiagramModelService } from '@angularflow/angular-adapter';
+
+@Component({
+  selector: 'sidebar-container',
+  templateUrl: `./sidebar.component.html`,
+  styleUrl: './sidebar.component.scss',
+})
+export class SidebarContainer {
+  private readonly modelService = inject(NgDiagramModelService);
+  private readonly selection = this.modelService.getSelection();
+  private readonly selectedNode = computed(() => this.selection().nodes[0]);
+
+  id = computed(() => this.selectedNode().id);
+  label = computed(() => this.selectedNode()?.data?.label ?? '');
+  isRotatable = computed(() => this.selectedNode()?.rotatable);
+  isResizable = computed(() => this.selectedNode()?.resizable);
+  isEnabled = computed(() => !!this.selectedNode());
+
+  onInput(value: string) {
+    this.modelService.updateNodeData<{ label: string }>(this.id(), {
+      label: value,
+    });
+  }
+
+  onIsResizableChange(value: boolean) {
+    this.modelService.updateNode(this.id(), { resizable: value });
+  }
+
+  onIsRotatableChange(value: boolean) {
+    this.modelService.updateNode(this.id(), { rotatable: value });
+  }
+}
