7776bd2a13b289fb4cb404da95a76d0e0db8737a
Kamil Fogel <kamil.fogel@synergycodes.com>
2025-08-24 22:06:59 +0200

    [Doc | Demo] Save state
    [Doc | Demo] Save state - status bar
    
    [Doc | Demo] Save state - changes
    
    [Doc | Demo] Save state - changes
    
    [Doc | Demo] Save state - changes v1
    
    [Doc | Demo] Save state - changes v2

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
 import starlightTypeDoc from 'starlight-typedoc';
 import starlightAutoSidebar from 'starlight-auto-sidebar';
 
---
+++ apps/docs/src/components/angular/examples/save-state/save-wrapper.component.ts
@@ -0,0 +1,30 @@
+import '@angular/compiler';
+import { Component } from '@angular/core';
+import {
+  NgDiagramContextComponent,
+  NgDiagramModelService,
+} from '@angularflow/angular-adapter';
+import { SaveStateExampleComponent } from './save-example.component';
+import { SaveStateService } from './save.service';
+
+@Component({
+  selector: 'save-state-wrapper',
+  imports: [NgDiagramContextComponent, SaveStateExampleComponent],
+  template: `
+    <ng-diagram-context>
+      <save-state-example></save-state-example>
+    </ng-diagram-context>
+  `,
+  styles: [
+    `
+      :host {
+        flex: 1;
+        display: flex;
+        position: relative;
+        flex-direction: column;
+      }
+    `,
+  ],
+  providers: [NgDiagramModelService, SaveStateService],
+})
+export class SaveStateWrapperComponent {}
