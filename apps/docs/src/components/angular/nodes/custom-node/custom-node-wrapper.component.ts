0ac56ffcadf8de67b5886efbac586062a63ff483
Kamil <69006267+kamilEf@users.noreply.github.com>
2025-09-03 13:01:23 +0200

    AF-178 [Doc] Custom Node (#238)
    * AF-178 [Doc] Custom Node
    
    * AF-178 [Doc] Custom Node
    
    * AF-178 [Doc] Custom Node
    
    * AF-178 [Doc] Custom Node

---
+++ apps/docs/src/components/angular/nodes/custom-node/custom-node-wrapper.component.ts
@@ -0,0 +1,67 @@
+import '@angular/compiler';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  NgDiagramComponent,
+  NgDiagramContextComponent,
+  type NgDiagramConfig,
+  type NgDiagramNodeTemplateMap,
+} from '@angularflow/angular-adapter';
+import { createSignalModel } from '@angularflow/angular-signals-model';
+
+import { CustomNodeComponent } from './node.component';
+
+@Component({
+  selector: 'customnode',
+  imports: [NgDiagramComponent, NgDiagramContextComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <ng-diagram-context>
+      <div class="not-content diagram">
+        <ng-diagram
+          [model]="model"
+          [config]="config"
+          [nodeTemplateMap]="nodeTemplateMap"
+        />
+      </div>
+    </ng-diagram-context>
+  `,
+  styles: [
+    `
+      :host {
+        flex: 1;
+        display: flex;
+
+        .diagram {
+          flex: 1;
+          display: flex;
+          height: 20rem;
+          font-family: 'Poppins', sans-serif;
+        }
+      }
+    `,
+  ],
+})
+export class CustomNodeWrapperComponent {
+  nodeTemplateMap: NgDiagramNodeTemplateMap = new Map([
+    ['myType', CustomNodeComponent],
+  ]);
+
+  config = {
+    zoom: {
+      max: 3,
+    },
+  } satisfies NgDiagramConfig;
+
+  model = createSignalModel({
+    nodes: [
+      {
+        id: '1',
+        position: { x: 100, y: 100 },
+        type: 'myType',
+        data: {},
+      },
+    ],
+    edges: [],
+    metadata: { viewport: { x: 0, y: 0, scale: 1.25 } },
+  });
+}
