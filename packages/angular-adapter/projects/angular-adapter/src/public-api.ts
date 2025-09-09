f8212f48d294a09147df26cc26871526f6114389
Tomasz Swistak <tomasz.swistak@synergycodes.com>
2025-09-09 14:15:27 +0200

    add explicit exports in public api

--- packages/angular-adapter/projects/angular-adapter/src/public-api.ts
+++ packages/angular-adapter/projects/angular-adapter/src/public-api.ts
@@ -9,24 +9,51 @@ export type {
   loggerMiddleware,
   Metadata,
   Middleware,
+  MiddlewareChain,
+  MiddlewaresConfigFromMiddlewares,
+  ModelAdapter,
   NgDiagramMath,
   Node,
   Point,
 } from '@angularflow/core';
-export * from './lib/components/background/ng-diagram-background.component';
-export * from './lib/components/context/ng-diagram-context.component';
-export * from './lib/components/diagram/ng-diagram.component';
-export * from './lib/components/edge-label/base-edge-label.component';
-export * from './lib/components/edge/base-edge/base-edge.component';
-export * from './lib/components/node/resize/ng-diagram-node-resize-adornment.component';
-export * from './lib/components/node/rotate/ng-diagram-node-rotate-adornment.component';
-export * from './lib/components/palette/item-preview/ng-diagram-palette-item-preview.component';
-export * from './lib/components/palette/item/ng-diagram-palette-item.component';
-export * from './lib/components/port/ng-diagram-port.component';
-export * from './lib/directives';
+export { NgDiagramBackgroundComponent } from './lib/components/background/ng-diagram-background.component';
+export { NgDiagramContextComponent } from './lib/components/context/ng-diagram-context.component';
+export { NgDiagramComponent } from './lib/components/diagram/ng-diagram.component';
+export { BaseEdgeLabelComponent } from './lib/components/edge-label/base-edge-label.component';
+export { NgDiagramBaseEdgeComponent } from './lib/components/edge/base-edge/base-edge.component';
+export { NgDiagramNodeResizeAdornmentComponent } from './lib/components/node/resize/ng-diagram-node-resize-adornment.component';
+export { NgDiagramNodeRotateAdornmentComponent } from './lib/components/node/rotate/ng-diagram-node-rotate-adornment.component';
+export { NgDiagramPaletteItemPreviewComponent } from './lib/components/palette/item-preview/ng-diagram-palette-item-preview.component';
+export { NgDiagramPaletteItemComponent } from './lib/components/palette/item/ng-diagram-palette-item.component';
+export { NgDiagramPortComponent } from './lib/components/port/ng-diagram-port.component';
+export { CursorPositionTrackerDirective } from './lib/directives/cursor-position-tracker/cursor-position-tracker.directive';
+export { NgDiagramGroupHighlightedDirective } from './lib/directives/group-highlighted/ng-diagram-group-highlighted.directive';
+export { KeyboardInputsDirective } from './lib/directives/input-events/keyboard-inputs/keyboard-inputs.directive';
+export { LinkingInputDirective } from './lib/directives/input-events/linking/linking.directive';
+export {
+  DiagramSelectionDirective,
+  EdgeSelectionDirective,
+  NodeSelectionDirective,
+} from './lib/directives/input-events/object-selection/object-selection.directive';
+export { PaletteDropDirective } from './lib/directives/input-events/palette-drop/palette-drop.directive';
+export { PanningDirective } from './lib/directives/input-events/panning/panning.directive';
+export { ZoomingPointerDirective } from './lib/directives/input-events/zooming/zooming-pointer.directive';
+export { ZoomingWheelDirective } from './lib/directives/input-events/zooming/zooming-wheel.directive';
+export { NodePositionDirective } from './lib/directives/node-position/node-position.directive';
+export { NgDiagramNodeSelectedDirective } from './lib/directives/node-selected/ng-diagram-node-selected.directive';
+export { NodeSizeDirective } from './lib/directives/node-size/node-size.directive';
+export { ViewportDirective } from './lib/directives/viewport/viewport.directive';
+export { ZIndexDirective } from './lib/directives/z-index/z-index.directive';
 export { initializeModel } from './lib/model/initializeModel';
-export * from './lib/public-services/ng-diagram-model.service';
-export * from './lib/public-services/ng-diagram-viewport.service';
-export * from './lib/public-services/ng-diagram.service';
-export * from './lib/types';
-export * from './lib/utils/create-middlewares';
+export { NgDiagramModelService } from './lib/public-services/ng-diagram-model.service';
+export { NgDiagramViewportService } from './lib/public-services/ng-diagram-viewport.service';
+export { NgDiagramService } from './lib/public-services/ng-diagram.service';
+export type { NgDiagramConfig } from './lib/types/config';
+export { NgDiagramEdgeTemplateMap } from './lib/types/edge-template-map';
+export type { NgDiagramEdgeTemplate } from './lib/types/edge-template-map';
+export type { PointerInputEvent } from './lib/types/event';
+export { NgDiagramNodeTemplateMap } from './lib/types/node-template-map';
+export type { NgDiagramGroupNodeTemplate, NgDiagramNodeTemplate } from './lib/types/node-template-map';
+export type { NgDiagramPaletteItem } from './lib/types/palette';
+export { createMiddlewares } from './lib/utils/create-middlewares';
+export type { AppMiddlewares } from './lib/utils/create-middlewares';
