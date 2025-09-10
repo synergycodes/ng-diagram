6112022428b53a050f9aa3238186d1d5d61cf5f9
Tomasz Swistak <tomasz.swistak@synergycodes.com>
2025-09-10 13:30:40 +0200

    moveViewport, moveViewportBy, zoom

--- packages/angular-adapter/projects/angular-adapter/src/lib/public-services/ng-diagram-viewport.service.ts
+++ packages/angular-adapter/projects/angular-adapter/src/lib/public-services/ng-diagram-viewport.service.ts
@@ -1,5 +1,5 @@
 import { computed, inject } from '@angular/core';
-import { MiddlewareChain } from '@angularflow/core';
+import { FlowCore, MiddlewareChain } from '@angularflow/core';
 import { FlowCoreProviderService } from '../services';
 import { NgDiagramModelService } from './ng-diagram-model.service';
 
@@ -16,4 +16,37 @@ export class NgDiagramViewportService<TMiddlewares extends MiddlewareChain = []>
    * Returns a computed signal for the scale that safely handles uninitialized state
    */
   scale = computed(() => this.modelService.metadata().viewport.scale || 1);
+
+  private get flowCore(): FlowCore<TMiddlewares> {
+    return this.flowCoreProvider.provide();
+  }
+
+  /**
+   * Moves the viewport to the specified coordinates.
+   * @param x The x-coordinate to move the viewport to.
+   * @param y The y-coordinate to move the viewport to.
+   */
+  moveViewport(x: number, y: number) {
+    this.flowCore.commandHandler.emit('moveViewport', { x, y });
+  }
+
+  /**
+   * Moves the viewport by the specified amounts.
+   * @param dx The amount to move the viewport in the x-direction.
+   * @param dy The amount to move the viewport in the y-direction.
+   */
+  moveViewportBy(dx: number, dy: number) {
+    this.flowCore.commandHandler.emit('moveViewportBy', { x: dx, y: dy });
+  }
+
+  /**
+   * Zooms the viewport by the specified factor.
+   * @param factor The factor to zoom by.
+   * @param center The center point to zoom towards.
+   */
+  zoom(factor: number, center?: { x: number; y: number } | undefined) {
+    const x = center?.x || this.viewport().x;
+    const y = center?.y || this.viewport().y;
+    this.flowCore.commandHandler.emit('zoom', { scale: factor, x, y });
+  }
 }
