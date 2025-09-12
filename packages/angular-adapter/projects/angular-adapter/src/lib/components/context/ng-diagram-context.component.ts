80df0ae17c7704ce61e14dc3ffa25ed1ca54c604
Piotr Blaszczyk <piotr.blaszczyk@synergycodes.com>
2025-09-12 09:45:53 +0200

    Register missing services in the ng diagram context

--- packages/angular-adapter/projects/angular-adapter/src/lib/components/context/ng-diagram-context.component.ts
+++ packages/angular-adapter/projects/angular-adapter/src/lib/components/context/ng-diagram-context.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { NgDiagramClipboardService } from '../../public-services/ng-diagram-clipboard.service';
 import { NgDiagramModelService } from '../../public-services/ng-diagram-model.service';
+import { NgDiagramSelectionService } from '../../public-services/ng-diagram-selection.service';
 import { NgDiagramViewportService } from '../../public-services/ng-diagram-viewport.service';
 import { NgDiagramService } from '../../public-services/ng-diagram.service';
 import { BatchResizeObserverService } from '../../services';
@@ -29,6 +31,8 @@ import { UpdatePortsService } from '../../services/update-ports/update-ports.ser
     BatchResizeObserverService,
     NgDiagramViewportService,
     NgDiagramModelService,
+    NgDiagramSelectionService,
+    NgDiagramClipboardService,
   ],
 })
 export class NgDiagramContextComponent {}
