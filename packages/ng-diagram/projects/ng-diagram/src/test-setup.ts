bd6eec8cd4eb0a8ed9662bb7e465cf9cb67e806f
Piotr Blaszczyk <piotr.blaszczyk@synergycodes.com>
2025-10-09 10:51:51 +0200

    import polyfills for test setup

--- packages/ng-diagram/projects/ng-diagram/src/test-setup.ts
+++ packages/ng-diagram/projects/ng-diagram/src/test-setup.ts
@@ -3,6 +3,7 @@ import '@analogjs/vitest-angular/setup-zone';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import { vi } from 'vitest';
+import './core/src/set.polyfill';
 
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
