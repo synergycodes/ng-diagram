18c0bcf10a6075a3a435d4d081b4b36c85b0c2bc
Kamil Fogel <kamil.fogel@synergycodes.com>
2025-10-10 17:11:58 +0200

    AF-258 Edge selection docs

--- apps/docs/src/components/angular/edges/edge-selection/dynamic-edge/dynamic-edge.component.ts
+++ apps/docs/src/components/angular/edges/edge-selection/dynamic-edge/dynamic-edge.component.ts
@@ -5,23 +5,30 @@ import {
   type NgDiagramEdgeTemplate,
 } from 'ng-diagram';
 
+// @section-start
 @Component({
+  // @mark-substring:[stroke]="edgeColor()"
+  // @mark-substring:[strokeWidth]="edgeWidth()"
+  // @mark-start
   template: `<ng-diagram-base-edge
     [edge]="edge()"
     [stroke]="edgeColor()"
     [strokeWidth]="edgeWidth()"
   />`,
+  // @mark-end
   imports: [NgDiagramBaseEdgeComponent],
 })
 export class DynamicEdgeComponent implements NgDiagramEdgeTemplate {
   edge = input.required<Edge>();
 
+  // @mark-start
   edgeColor = computed(() => {
     const edge = this.edge();
     if (edge.selected) return 'yellow';
     if (edge.temporary) return 'gray';
     return 'black';
   });
-
   edgeWidth = computed(() => (this.edge().selected ? 3 : 2));
+  // @mark-end
 }
+// @section-end
