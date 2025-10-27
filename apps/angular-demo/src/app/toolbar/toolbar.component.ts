7d3d050d72ea67c2c0f3998280862480dbe3a855
Piotr Blaszczyk <piotr.blaszczyk@synergycodes.com>
2025-10-27 13:55:43 +0100

    Improve transactions documentation

--- apps/angular-demo/src/app/toolbar/toolbar.component.ts
+++ apps/angular-demo/src/app/toolbar/toolbar.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, output } from '@angular/core';
-import { NgDiagramSelectionService, NgDiagramService, NgDiagramViewportService } from 'ng-diagram';
+import { Component, computed, inject, output } from '@angular/core';
+import {
+  NgDiagramModelService,
+  NgDiagramSelectionService,
+  NgDiagramService,
+  NgDiagramViewportService,
+} from 'ng-diagram';
+import { nodeTemplateMap, NodeTemplateType } from '../data/node-template';
 
 @Component({
   selector: 'app-toolbar',
@@ -12,8 +18,13 @@ export class ToolbarComponent {
   private readonly ngDiagramService: NgDiagramService = inject(NgDiagramService);
   private readonly ngDiagramSelectionService = inject(NgDiagramSelectionService);
   private readonly ngDiagramViewportService = inject(NgDiagramViewportService);
+  private readonly ngDiagramModelService = inject(NgDiagramModelService);
+
+  private readonly nodeTypes = Array.from(nodeTemplateMap.keys()) as NodeTemplateType[];
 
   toggleDebugModeClick = output<void>();
+
+  isNodeSelected = computed(() => this.ngDiagramSelectionService.selection().nodes.length > 0);
 
   onToggleDebugModeClick(): void {
     const { debugMode } = this.ngDiagramService.getConfig();
@@ -35,5 +46,24 @@ export class ToolbarComponent {
     if (node) {
       this.ngDiagramViewportService.centerOnNode(node.id);
     }
+  }
+
+  onChangeNodeTypeClick() {
+    const selectedNodes = this.ngDiagramSelectionService.selection().nodes;
+
+    if (selectedNodes.length === 0) {
+      return;
+    }
+
+    selectedNodes.forEach((node) => {
+      let newType: NodeTemplateType;
+      do {
+        newType = this.nodeTypes[Math.floor(Math.random() * this.nodeTypes.length)];
+      } while (newType === node.type && this.nodeTypes.length > 1);
+
+      this.ngDiagramModelService.updateNode(node.id, {
+        type: newType,
+      });
+    });
   }
 }
