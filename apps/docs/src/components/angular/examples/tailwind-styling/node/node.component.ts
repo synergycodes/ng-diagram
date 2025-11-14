70903b99dc1d6b44a5313db8edc1670b91453f3c
Kamil <69006267+kamilEf@users.noreply.github.com>
2025-11-14 14:25:19 +0100

    AF-277 Docs Tailwind CSS (#436)
    * AF-277 Tailwind
    
    AF-277 Tailwind
    
    AF-277 Tailwind
    
    * AF-277 Tailwind
    
    * AF-277 Tailwind
    
    * AF-277 Tailwind
    
    * AF-277 Tailwind

---
+++ apps/docs/src/components/angular/examples/tailwind-styling/node/node.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component, input, model } from '@angular/core';
+import {
+  NgDiagramNodeResizeAdornmentComponent,
+  NgDiagramNodeRotateAdornmentComponent,
+  NgDiagramNodeSelectedDirective,
+  NgDiagramPortComponent,
+  type NgDiagramNodeTemplate,
+  type Node,
+} from 'ng-diagram';
+
+type CustomDataType = {
+  name: string;
+  description: string;
+  tooltip: string;
+};
+
+@Component({
+  imports: [
+    NgDiagramNodeRotateAdornmentComponent,
+    NgDiagramPortComponent,
+    NgDiagramNodeResizeAdornmentComponent,
+    CommonModule,
+  ],
+  templateUrl: './node.component.html',
+  styleUrls: ['./node.component.scss'],
+  hostDirectives: [
+    { directive: NgDiagramNodeSelectedDirective, inputs: ['node'] },
+  ],
+  host: {
+    '[class.ng-diagram-port-hoverable-over-node]': 'true',
+    class: 'flex w-full h-full rounded-lg bg-white dark:bg-neutral-800',
+  },
+})
+export class NodeComponent implements NgDiagramNodeTemplate<CustomDataType> {
+  text = model<string>('');
+  node = input.required<Node<CustomDataType>>();
+
+  selectedState: string = 'Inactive';
+
+  onStateChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedState = selectElement.value;
+  }
+}
